7dd5cd696a40e62d30130245a33d841222946fc8
soconzan <bin00inbubble@gmail.com>
2025-06-10 02:32:41 +0900

    back 수정

--- backend/app/models/employee_model.py
+++ backend/app/models/employee_model.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, Enum
-from sqlalchemy.orm import relationship
-from app.database import Base
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from ..core.database import Base
 import enum
 
 class UserTypeEnum(str, enum.Enum):
@@ -11,16 +11,15 @@ class UserTypeEnum(str, enum.Enum):
 class Employee(Base):
     __tablename__ = "employees"
 
-    employeeId = Column(String(20), primary_key=True, index=True)
-    employeeNum = Column(Integer, unique=True, nullable=False, index=True)
-    password = Column(String(256), nullable=False)
-    name = Column(String(50), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    phone = Column(String(20), nullable=False)
-    # departmentId = Column(Integer, ForeignKey("departments.departmentId"), nullable=False)
-    departmentId = Column(Integer, nullable=True)
-    position = Column(String(20), nullable=False)
-    userType = Column(Enum(UserTypeEnum), nullable=False)
+    employeeId: Mapped[str] = mapped_column(String(20),  primary_key=True, index=True)
+    employeeNum: Mapped[int] = mapped_column(Integer,       unique=True, nullable=False, index=True)
+    password: Mapped[str] = mapped_column(String(256), nullable=False)
+    name: Mapped[str] = mapped_column(String(50),  nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20),  nullable=False)
+    departmentId: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    position: Mapped[str] = mapped_column(String(20),  nullable=False)
+    userType: Mapped[UserTypeEnum] = mapped_column(Enum(UserTypeEnum), nullable=False)
 
     # 관계 설정 (부서 테이블과 연결)
     # department = relationship("Department", back_populates="employees")
---
+++ backend/app/services/employee_service.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from ..daos.employee_dao import EmployeeDAO
+from ..schemas.employee_schema import EmployeeCreate
+from ..models.employee_model import Employee
+
+class EmployeeService:
+    def __init__(self, employee_dao: EmployeeDAO):
+        self.employee_dao = employee_dao
+
+    # 직원 등록
+    def register_employee(self, employee_data: EmployeeCreate) -> Employee:
+        new_employee = Employee(**employee_data.model_dump())
+
+        created_employee = self.employee_dao.create_employee(new_employee)
+        return created_employee
+    
+    # 직원 목록 조회
+    def get_employee_list(self, skip:int = 0, limit: int = 100) -> List[Employee]:
+        employees = self.employee_dao.get_employees(skip= skip, limit= limit)
+        return employees
+    
+    # 상세 조회
+    def get_employee(self, emp_num: int) -> Optional[Employee]:
+        emp = self.employee_dao.get_employee(emp_num)
+        return emp
